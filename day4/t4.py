e49a9c2493624736acabfa47368341ed05197b84
jaakko <jaakko.junttu@tuni.fi>
2022-12-04 14:57:57 +0200

    day four done like it was nothing

---
+++ day4/t4.py
@@ -0,0 +1,33 @@
+
+
+with open("t4.txt", 'r') as file:
+        data = [[e[0].split("-"), e[1].strip().split("-")] 
+                for e in [i.split(",") for i in file.readlines()]]
+
+
+def part_one():
+
+    print(data[0])
+
+    def rule1(i):
+        ff, fs, sf, ss = int(i[0][0]), int(i[0][1]), int(i[1][0]), int(i[1][1])
+        return True if ff <= sf and fs >= ss or sf <= ff and ss >= fs else False
+    
+    return list(map(lambda i : 1 if rule1(i) else 0, data)).count(1)
+
+    
+def part_two():
+
+    def rule2(i):
+        ff, fs, sf, ss = int(i[0][0]), int(i[0][1]), int(i[1][0]), int(i[1][1])
+        return False if fs < sf or ss < ff else True
+
+    return list(map(lambda i : 1 if rule2(i) else 0, data)).count(1)
+    
+
+def main():
+
+    print(f"PART 1: {part_one()} \nPART 2: {part_two()}")
+
+if __name__ == "__main__":
+    main()
