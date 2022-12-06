fe6808a1016d062595f9979bd4b78f2b672379cd
jaakko <jaakko.junttu@tuni.fi>
2022-12-06 16:46:52 +0200

    day 6 done with grace

---
+++ day6/t6.py
@@ -0,0 +1,15 @@
+
+
+def parts(n):
+
+    line = open("t6.txt", 'r').readline()
+    return min([idx + 1 for idx in range(n, len(line)) if \
+                                            len(set(line[idx-(n-1):idx+1])) == n])
+
+
+def main():
+
+    print(f"part one: {parts(4)}\npart two: {parts(14)}")
+
+if __name__ == "__main__":
+    main()
