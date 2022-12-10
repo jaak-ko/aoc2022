d26056800deba5ab3d2cbeb943285d4e798ff0b7
jaakko <jaakko.junttu@tuni.fi>
2022-12-10 19:05:46 +0200

    day 10 some optimizing

--- day10/t10.py
+++ day10/t10.py
@@ -5,28 +5,26 @@ with open("t10.txt", 'r') as f:
 
 def part_one():
 
+    # answer = 12880
     def check(cycle): return cycle in [20, 60, 100, 140, 180, 220]
 
     x = 1
     solution = 0
     cycle = 0
-    cycle_checked = False
 
     for idx, row in enumerate(data):
 
         if row[0] == "addx":
             for _ in range(2):
                 cycle += 1
-                cycle_checked = False
-                if check(cycle) and not cycle_checked:
+                if check(cycle):
                     solution += x * cycle
                     cycle_checked = True
             x += int(row[-1])
         else:
+
             if check(cycle) and not cycle_checked:
                 solution += x * cycle
-                cycle_checked = True
-
             cycle += 1
             cycle_checked = False
 
@@ -39,16 +37,14 @@ def part_two():
     pixels = ""
     for idx, row in enumerate(data):
 
-        if cycle == 39:
-            r = ""
         if row[0] == "addx":
             for _ in range(2):
                 pixels += '#'if cycle % 40 in range(x-1, x+2) else '.'
                 cycle += 1
             x += int(row[-1])
-        else:
-            pixels += '#'if cycle % 40 in range(x-1, x+2) else '.'
-            cycle += 1
+            continue
+        pixels += '#'if cycle % 40 in range(x-1, x+2) else '.'
+        cycle += 1
 
     for idx, c in enumerate([*pixels]):
         print(c, end="") if (idx + 1) % 40 != 0 or idx == 0 else print(c)
